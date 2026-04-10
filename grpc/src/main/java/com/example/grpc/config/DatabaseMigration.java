f640de41a5e5daef2bfbc9f33e29ec35076cd1ae
Jonah Doll <187488776+jonahdoll@users.noreply.github.com>
2026-04-10 14:58:04 +0200

    Implementierung einer eigenen DatenbankMigration Klasse.

---
+++ grpc/src/main/java/com/example/grpc/config/DatabaseMigration.java
@@ -0,0 +1,32 @@
+package com.example.grpc.config;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.flywaydb.core.Flyway;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DatabaseMigration {
+    private static final Logger logger = LoggerFactory.getLogger(DatabaseMigration.class);
+
+
+    public static void migrate() {
+        Dotenv dotenv = Dotenv.load();
+        boolean shouldClean = Boolean.parseBoolean(dotenv.get("DB_CLEAN", "false"));
+
+        logger.info("Starte Datenbank Migration...");
+
+        Flyway flyway = Flyway.configure()
+                .dataSource(DatabaseConfig.getInstance())
+                .cleanDisabled(false)
+                .locations("classpath:db/migration")
+                .load();
+
+        if (shouldClean) {
+            logger.info("Die Datenbank wird zurückgesetzt.");
+            flyway.clean();
+        }
+
+        flyway.migrate();
+        logger.info("Datenbank Migration erfolgreich abgeschlossen.");
+    }
+}
