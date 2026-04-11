640f4bee54d4afb277728d8a58d6f85fee287b47
Jonah Doll <187488776+jonahdoll@users.noreply.github.com>
2026-04-11 12:06:13 +0200

    Hinzufügen der DatabaseMigration.

---
+++ zahlungssystem/src/main/java/com/example/zahlungsystem/config/DatabaseMigration.java
@@ -0,0 +1,32 @@
+package com.example.zahlungsystem.config;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.flywaydb.core.Flyway;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DatabaseMigration {
+    private static final Logger logger = LoggerFactory.getLogger(DatabaseMigration.class);
+
+
+    public static void migrate() {
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+        boolean shouldClean = Boolean.parseBoolean(dotenv.get("ZAHLUNGSSYSTEM_DB_CLEAN", "false"));
+
+        logger.info("Starte Datenbank Migration...");
+
+        Flyway flyway = Flyway.configure()
+                .dataSource(DatabaseConfig.getInstance())
+                .cleanDisabled(false)
+                .locations("classpath:db/migration")
+                .load();
+
+        if (shouldClean) {
+            logger.info("Die Datenbank wird zurückgesetzt.");
+            flyway.clean();
+        }
+
+        flyway.migrate();
+        logger.info("Datenbank Migration erfolgreich abgeschlossen.");
+    }
+}
