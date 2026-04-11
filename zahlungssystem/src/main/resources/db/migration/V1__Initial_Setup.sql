b2e84fb7b1a244250f0e23283e3bad30dc7445ad
Jonah Doll <187488776+jonahdoll@users.noreply.github.com>
2026-04-11 12:07:41 +0200

    Hinzufügen des sql schemas.

---
+++ zahlungssystem/src/main/resources/db/migration/V1__Initial_Setup.sql
@@ -0,0 +1,12 @@
+CREATE TYPE zahlungs_status_type AS ENUM ('OFFEN', 'IN_BEARBEITUNG', 'ABGESCHLOSSEN', 'FEHLGESCHLAGEN');
+
+CREATE TABLE zahlungsauftraege (
+    zahlungs_referenz VARCHAR(255) PRIMARY KEY,
+    betrag DECIMAL(15, 2) NOT NULL CHECK (betrag > 0),
+    iban VARCHAR(34) NOT NULL,
+    faelligkeitsdatum TIMESTAMP NOT NULL,
+    status zahlungs_status_type NOT NULL
+);
+
+CREATE INDEX idx_auftraege_zeitstempel ON zahlungsauftraege(faelligkeitsdatum);
+CREATE INDEX idx_auftraege_status ON zahlungsauftraege(status);
